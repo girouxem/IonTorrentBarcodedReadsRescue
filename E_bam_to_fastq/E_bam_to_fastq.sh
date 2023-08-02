ca2288609373ca868b59e3ad4398b13dcd97063a
girouxem <emily.giroux.science@gmail.com>
2023-08-02 12:28:17 -0400

    adding process steps

---
+++ E_bam_to_fastq/E_bam_to_fastq.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+            #$ -S /bin/bash
+            argc=$#
+            requiredArgc=0
+            if [ $argc -ne $requiredArgc ]; then
+            echo './test_mkdir.sh'
+            exit 1
+            fi
+            
+            prefixInFiles=E_bam_to_fastq
+suffixInFiles=.sub
+for (( i = 1; i <= 31 ; i++ )); do 
+            # keep track of what is going on...
+            echo 'Treating file'  $prefixInFiles$i$suffixInFiles
+            # define a script name that will be submited to the queue
+            qsubFile=$prefixInFiles$i$suffixInFiles
+            # make the script executable
+            chmod a+x $qsubFile
+            # submit the script to the queue
+            qsub -cwd $qsubFile
+            done
